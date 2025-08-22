c455b748aa44077ee3f60bddf2ba5db775871f1d
Tasnim Mahmud <deaddealer101@gmail.com>
2025-08-22 15:42:25 +1000

    1

---
+++ src/Grid.java
@@ -0,0 +1,31 @@
+import java.awt.*;
+
+public class Grid {
+    //fields
+    Cell[][] cells=new Cell[20][20];
+
+
+    //constructor
+    public Grid(){
+        for(int i = 0; i<cells.length;i++){
+            for (int j = 0; j<cells[i].length;j++){
+                cells[i][j]=new Cell(10+35*i,10+35*j);
+
+            }
+        }
+
+    }
+    //Method
+    public void paint(Graphics g, Point mousePos){
+        for(int i = 0; i<cells.length;i++){
+            for (int j = 0; j<cells[i].length;j++){
+                cells[i][j].paint(g, mousePos);
+
+                   
+            }
+                
+        }
+
+    }
+    
+}
